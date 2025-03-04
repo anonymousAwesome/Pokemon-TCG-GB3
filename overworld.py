50e4ef4538c5af68fc96612795ab9681c0e0b31d
Brendan J. <brendankaizen@gmail.com>
2025-03-03 23:49:42 -0800

    moved temp trigger list into a class to enable function passing

--- overworld.py
+++ overworld.py
@@ -32,15 +32,22 @@ current_dialogue=ui.Dialogue(screen,"")
 
 collision_manager=map_managers.CollisionManager(map_holder.current_map.bg_image, player_character, map_holder.current_map.obstacles)
 
-def generate_temp_trigger_list(map_holder,player_character):
+class TempTriggerList():
     '''called when the player moves into a new map, so I'm not 
     instantiating the trigger class 60 times a second.'''
-    temp_list=[]
-    for trigger in map_holder.current_map.step_exit_triggers:
-        temp_list.append(trigger(player_character))
-    return temp_list
+    def __init__(self,temp_list=[]):
+        self.temp_list=temp_list
 
-temp_trigger_list=generate_temp_trigger_list(map_holder,player_character)
+    def generate_temp_trigger_list(self,map_holder,player_character):
+        temp_list=[]
+        for trigger in map_holder.current_map.step_exit_triggers:
+            temp_list.append(trigger(player_character))
+        self.temp_list=temp_list
+
+
+temp_trigger_list=TempTriggerList()
+
+temp_trigger_list.generate_temp_trigger_list(map_holder,player_character)
 
 triggers=map_managers.MapTriggerManager(screen, player_character, map_holder.current_map, current_dialogue)
 
@@ -78,13 +85,13 @@ if __name__=="__main__":
                 current_dialogue=temp_dialogue
 
             #if player steps on an exit trigger, change the current map and player location, then update the map managers.
-            for trigger in temp_trigger_list:
+            for trigger in temp_trigger_list.temp_list:
                 if trigger.rect.contains(player_character.rect):
                     player_character.pixels_remaining=0
                     trigger.step_on_exit(map_holder,screen)
                     collision_manager.__init__(map_holder.current_map.bg_image,player_character,map_holder.current_map.obstacles)
                     triggers.__init__(screen,player_character,map_holder.current_map,current_dialogue)
-                    temp_trigger_list=generate_temp_trigger_list(map_holder,player_character)
+                    temp_trigger_list.generate_temp_trigger_list(map_holder,player_character)
 
         elif current_dialogue:
             current_dialogue.render(event_list)
