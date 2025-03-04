ed1fac575f1f77960670a62f54ee78b325dc31dd
Brendan J. <brendankaizen@gmail.com>
2025-03-04 00:00:02 -0800

    renamed "temp trigger list" to "temp exit list"

--- overworld.py
+++ overworld.py
@@ -32,22 +32,22 @@ current_dialogue=ui.Dialogue(screen,"")
 
 collision_manager=map_managers.CollisionManager(map_holder.current_map.bg_image, player_character, map_holder.current_map.obstacles)
 
-class TempTriggerList():
+class TempExitList():
     '''called when the player moves into a new map, so I'm not 
     instantiating the trigger class 60 times a second.'''
     def __init__(self,temp_list=[]):
         self.temp_list=temp_list
 
-    def generate_temp_trigger_list(self,map_holder,player_character):
+    def generate_temp_exit_list(self,map_holder,player_character):
         temp_list=[]
         for trigger in map_holder.current_map.step_exit_triggers:
             temp_list.append(trigger(player_character))
         self.temp_list=temp_list
 
 
-temp_trigger_list=TempTriggerList()
+temp_exit_list=TempExitList()
 
-temp_trigger_list.generate_temp_trigger_list(map_holder,player_character)
+temp_exit_list.generate_temp_exit_list(map_holder,player_character)
 
 triggers=map_managers.MapTriggerManager(screen, player_character, map_holder.current_map, current_dialogue)
 
@@ -82,16 +82,16 @@ if __name__=="__main__":
             #replace the empty current dialogue with a new one if the map trigger manager says the player interacted with the object.
             temp_dialogue=triggers.interact_object_make_dialogue(event_list)
             if temp_dialogue:
-                current_dialogue=temp_dialogue
+                current_dialogue.__init__(screen,temp_dialogue)
 
             #if player steps on an exit trigger, change the current map and player location, then update the map managers.
-            for trigger in temp_trigger_list.temp_list:
+            for trigger in temp_exit_list.temp_list:
                 if trigger.rect.contains(player_character.rect):
                     player_character.pixels_remaining=0
                     trigger.step_on_exit(map_holder,screen)
                     collision_manager.__init__(map_holder.current_map.bg_image,player_character,map_holder.current_map.obstacles)
                     triggers.__init__(screen,player_character,map_holder.current_map,current_dialogue)
-                    temp_trigger_list.generate_temp_trigger_list(map_holder,player_character)
+                    temp_exit_list.generate_temp_exit_list(map_holder,player_character)
 
         elif current_dialogue:
             current_dialogue.render(event_list)
